07910566a12fb176176cd292c5549dcd36182306
Henry B <noelys215@gmail.com>
2022-08-16 21:22:32 -0400

    add routes

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import './navigation.styles.scss';
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+
+const Navigation = () => {
+	return (
+		<>
+			<div className="navigation">
+				<Link to="/" className="logo-container">
+					<CrownLogo className="logo" />
+				</Link>
+				<div className="nav-links-container">
+					<Link className="nav-link" to="">
+						SHOP
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</>
+	);
+};
+
+export default Navigation;
